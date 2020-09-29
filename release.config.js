f1c9b6b915054ae64bdf982c68b007705c4caf2c
Christian Rackerseder <git@echooff.de>
2020-09-29 20:26:17 +0200

    ci: configure semantic-release

---
+++ release.config.js
@@ -0,0 +1,11 @@
+'use strict';
+
+module.exports = {
+  branches: ['main'],
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/git',
+  ],
+};
