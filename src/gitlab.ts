919524b2cf0ca02f51c9b4e14d4aa8fd09527328
Christian Rackerseder <git@echooff.de>
2020-09-30 16:05:50 +0200

    feat: increase number of items to 100 in pipelines list

--- src/gitlab.ts
+++ src/gitlab.ts
@@ -10,7 +10,7 @@ interface Arguments {
 }
 
 export async function listPipelines({ get, gitlabUrl, projectId, accessToken }: Arguments): Promise<L.List<Pipeline>> {
-  const url = urlcat(gitlabUrl, '/api/v4/projects/:id/pipelines', { id: projectId });
+  const url = urlcat(gitlabUrl, '/api/v4/projects/:id/pipelines', { id: projectId, per_page: 100 });
   const pipelines = await get(url, accessToken);
   return L.from(pipelines);
 }
--- test/unit/gitlab.test.ts
+++ test/unit/gitlab.test.ts
@@ -14,14 +14,22 @@ suite('gitlab', function () {
   test('listPipelines() creates the correct API URL', async function () {
     const get = sinon.fake.resolves([]);
     await callListPipelines(get);
-    sinon.assert.calledWithMatch(get, 'https://gitlab.my-domain.io/api/v4/projects/42/pipelines');
+    sinon.assert.calledWith(
+      get,
+      'https://gitlab.my-domain.io/api/v4/projects/42/pipelines?per_page=100',
+      sinon.match.string,
+    );
   });
 
   test('listPipelines() creates the correct API URL even when the given GitLab URL has a trailing slash', async function () {
     const get = sinon.fake.resolves([]);
     const gitlabUrl = 'https://gitlab.my-domain.io/';
     await callListPipelines(get, gitlabUrl);
-    sinon.assert.calledWithMatch(get, 'https://gitlab.my-domain.io/api/v4/projects/42/pipelines');
+    sinon.assert.calledWith(
+      get,
+      'https://gitlab.my-domain.io/api/v4/projects/42/pipelines?per_page=100',
+      sinon.match.string,
+    );
   });
 
   test('listPipelines() passes the given access token to get() function', async function () {
