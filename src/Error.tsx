28f695d151081fde02f601d81d8b311af7f389f5
Christian Rackerseder <git@echooff.de>
2020-09-30 10:40:05 +0200

    feat: create <Error /> component

---
+++ src/Error.tsx
@@ -0,0 +1,13 @@
+import React, { FunctionComponent, useEffect } from 'react';
+import { Text } from 'ink';
+
+export interface ErrorProps {
+  exit: () => void;
+}
+
+export const Error: FunctionComponent<ErrorProps> = (props) => {
+  useEffect(() => {
+    props.exit();
+  });
+  return <Text color="red">{props.children}</Text>;
+};
---
+++ test/unit/Error.test.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import sinon from 'sinon';
+import { assert } from 'chai';
+import { render } from 'ink-testing-library';
+import { Error, ErrorProps } from '../../src/Error';
+
+function renderError(text: string, overrides: Partial<ErrorProps> = {}) {
+  const props: ErrorProps = {
+    exit: sinon.fake(),
+    ...overrides,
+  };
+  return render(<Error exit={props.exit}>{text}</Error>);
+}
+
+suite('<Error />', function () {
+  test('renders a red error message', function () {
+    const { lastFrame } = renderError('Test error');
+    const actual = lastFrame();
+    const expected = '\u001b[31mTest error\u001b[39m';
+    assert.equal(actual, expected);
+  });
+
+  test('calls given exit() callback after it was rendered', function () {
+    const exit = sinon.fake();
+    const { unmount } = renderError('Test error', { exit });
+    unmount();
+    sinon.assert.calledOnce(exit);
+  });
+});
