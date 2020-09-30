4f00fa99e203563375d99d5509786728fb87c9af
Christian Rackerseder <git@echooff.de>
2020-09-30 11:14:35 +0200

    feat: require project id to be set as command line argument

--- src/bin.tsx
+++ src/bin.tsx
@@ -8,11 +8,12 @@ program
   .name('gitlab-pipeline-deleter')
   .description('Deletes old GitLab pipelines')
   .arguments('<gitlab-url>')
+  .arguments('<project-id>')
   .arguments('<access-token>')
   .option('-d --days <days>', 'older than days', '30')
-  .action((gitlabUrl: string, accessToken: string) => {
+  .action((gitlabUrl: string, projectId: string, accessToken: string) => {
     const days = parseInt(program.days, 10);
-    render(<App gitlabUrl={gitlabUrl} accessToken={accessToken} days={days} />);
+    render(<App gitlabUrl={gitlabUrl} projectId={projectId} accessToken={accessToken} days={days} />);
   });
 
 program.parse(process.argv);
