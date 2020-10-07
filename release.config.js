4aa1171d56c1429c3b5487760aa38ce0f91ab1ff
Christian Rackerseder <git@echooff.de>
2020-10-07 09:14:24 +0200

    fix: change order of semantic-release plugins

--- release.config.js
+++ release.config.js
@@ -6,8 +6,8 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/changelog',
-    '@semantic-release/git',
     '@semantic-release/npm',
+    '@semantic-release/git',
     '@semantic-release/github',
   ],
 };
