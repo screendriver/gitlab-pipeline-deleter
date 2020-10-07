a047ec90d4d475e92ceae0f50f429547a397ba15
Christian Rackerseder <git@echooff.de>
2020-10-07 08:10:15 +0200

    tests: configure mutation tests

---
+++ stryker.conf.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = function (config) {
+  config.set({
+    mutator: 'typescript',
+    tsconfigFile: 'tsconfig.base.json',
+    transpilers: ['typescript'],
+    packageManager: 'npm',
+    reporters: ['html', 'clear-text', 'progress'],
+    testRunner: 'mocha',
+    testFramework: 'mocha',
+    mochaOptions: {
+      config: './.mocharc.js',
+      spec: ['test/unit/**/*.test.*'],
+    },
+    mutate: ['src/**/*.ts?(x)', '!src/bin.tsx', '!src/network.ts'],
+    tempDirName: 'target/.stryker-tmp',
+    htmlReporter: { baseDir: 'target/mutation-report/html' },
+    thresholds: {
+      break: 94,
+    },
+    coverageAnalysis: 'off',
+  });
+};
