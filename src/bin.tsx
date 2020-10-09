f5126a3e933c28de1f87c55a32cd882ec60f027e
Christian Rackerseder <git@echooff.de>
2020-10-09 10:53:14 +0200

    feat(config): read arguments from CLI and config file
    BREAKING CHANGE: CLI arguments are now optional instead of required

--- src/bin.tsx
+++ src/bin.tsx
@@ -2,9 +2,12 @@
 import React from 'react';
 import { createCommand } from 'commander';
 import { render } from 'ink';
-import { Main } from './Main';
+import { cosmiconfig } from 'cosmiconfig';
+import { App } from './App';
+import { Error } from './Error';
 import { getRequest, deleteRequest } from './network';
 import { listPipelines, filterPipelinesByDate, deletePipeline } from './gitlab';
+import { CliArguments, loadConfig, mergeCliArgumentsWithConfig } from './config';
 
 function exit() {
   process.exitCode = 1;
@@ -16,36 +19,61 @@ program
   .storeOptionsAsProperties(false)
   .passCommandToAction(false)
   .description('Deletes old GitLab pipelines')
-  .arguments('<gitlab-url>')
-  .arguments('<project-id>')
-  .arguments('<access-token>')
+  .arguments('[gitlab-url]')
+  .arguments('[project-id]')
+  .arguments('[access-token]')
   .option('-d --days <days>', 'older than days', '30')
   .option('--trace', 'show stack traces for errors when possible', false)
-  .action((gitlabUrl: string, projectId: string, accessToken: string, options: Record<string, unknown>) => {
-    const parsedProjectId = parseInt(projectId, 10);
-    const days = typeof options.days === 'string' ? parseInt(options.days, 10) : 30;
-    const startDate = new Date();
-    const listPipelinesFunction = listPipelines({ getRequest, gitlabUrl, projectId: parsedProjectId, accessToken });
-    const deletePipelineFunction = deletePipeline({
-      deleteRequest,
-      gitlabUrl,
-      projectId: parsedProjectId,
-      accessToken,
-    });
-    render(
-      <Main
-        gitlabUrl={gitlabUrl}
-        projectId={parsedProjectId}
-        accessToken={accessToken}
-        days={days}
-        startDate={startDate}
-        exit={exit}
-        listPipelines={listPipelinesFunction}
-        filterPipelinesByDate={filterPipelinesByDate}
-        deletePipeline={deletePipelineFunction}
-        showStackTraces={options.trace === true}
-      />,
-    );
-  });
+  .action(
+    async (
+      gitlabUrlArgument: string | undefined,
+      projectIdArgument: string | undefined,
+      accessTokenArgument: string | undefined,
+      options: Record<string, unknown>,
+    ) => {
+      const cliArguments: CliArguments = {
+        gitlabUrl: gitlabUrlArgument,
+        projectId: projectIdArgument ? parseInt(projectIdArgument, 10) : undefined,
+        accessToken: accessTokenArgument,
+        days: typeof options.days === 'string' ? parseInt(options.days, 10) : 30,
+        trace: options.trace === true,
+      };
+      const explorer = cosmiconfig('gitlab-pipeline-deleter');
+      const config = await loadConfig('./glpdrc.js', explorer);
+      const glpdArguments = mergeCliArgumentsWithConfig(cliArguments, config);
+      if (!glpdArguments.success) {
+        render(<Error exit={exit}>Missing or invalid arguments</Error>);
+        return;
+      }
+      const { gitlabUrl, projectId, accessToken, days, trace } = glpdArguments.data;
+      const startDate = new Date();
+      const listPipelinesFunction = listPipelines({
+        getRequest,
+        gitlabUrl: gitlabUrl,
+        projectId: projectId,
+        accessToken: accessToken,
+      });
+      const deletePipelineFunction = deletePipeline({
+        deleteRequest,
+        gitlabUrl: gitlabUrl,
+        projectId: projectId,
+        accessToken: accessToken,
+      });
+      render(
+        <App
+          gitlabUrl={gitlabUrl}
+          projectId={projectId}
+          accessToken={accessToken}
+          days={days}
+          startDate={startDate}
+          exit={exit}
+          listPipelines={listPipelinesFunction}
+          filterPipelinesByDate={filterPipelinesByDate}
+          deletePipeline={deletePipelineFunction}
+          showStackTraces={trace}
+        />,
+      );
+    },
+  );
 
 program.parse(process.argv);
