3bb1da9f4e607360a6de5c15513ce64da67fe1ff
Christian Rackerseder <git@echooff.de>
2020-10-19 08:06:56 +0200

    test(mutation): use command instead of mocha runner

--- stryker.conf.js
+++ stryker.conf.js
@@ -1,24 +1,18 @@
 'use strict';
 
-module.exports = function (config) {
-  config.set({
-    mutator: 'typescript',
-    tsconfigFile: 'tsconfig.base.json',
-    transpilers: ['typescript'],
-    packageManager: 'npm',
-    reporters: ['html', 'clear-text', 'progress'],
-    testRunner: 'mocha',
-    testFramework: 'mocha',
-    mochaOptions: {
-      config: './.mocharc.js',
-      spec: ['test/unit/**/*.test.*'],
-    },
-    mutate: ['src/**/*.ts?(x)', '!src/bin.tsx', '!src/network.ts'],
-    tempDirName: 'target/.stryker-tmp',
-    htmlReporter: { baseDir: 'target/mutation-report/html' },
-    thresholds: {
-      break: 92,
-    },
-    coverageAnalysis: 'off',
-  });
+/**
+ * @type {import('@stryker-mutator/api/core').StrykerOptions}
+ */
+module.exports = {
+  checkers: ['typescript'],
+  mutate: ['src/**/*.ts?(x)', '!src/bin.tsx', '!src/network.ts'],
+  testRunner: 'command',
+  commandRunner: { command: 'npm run test:unit' },
+  tempDirName: 'target/.stryker-tmp',
+  reporters: ['html', 'clear-text', 'dots'],
+  htmlReporter: { baseDir: 'target/mutation-report/html' },
+  thresholds: {
+    break: 90,
+  },
+  coverageAnalysis: 'off',
 };
