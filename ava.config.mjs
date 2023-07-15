b8fe4970f4eeca343b7ba4eb6e49665d3455a942
Christian Rackerseder <git@echooff.de>
2023-07-16 00:21:10 +0200

    Configure tests to run directly against transpiled JavaScript file

--- ava.config.mjs
+++ ava.config.mjs
@@ -1,5 +1,11 @@
 export default {
     files: ['./test/unit/**/*.test.{ts,tsx}'],
-    extensions: ['ts', 'tsx'],
-    require: ['ts-node/register/transpile-only'],
+    typescript: {
+        extensions: ['ts', 'tsx'],
+        rewritePaths: {
+            'src/': 'target/src/',
+            'test/': 'target/test/',
+        },
+        compile: false,
+    },
 };
