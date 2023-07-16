9e3a5a8b28f8182a5fb94c99cbcf2ede3a93e1a9
Christian Rackerseder <git@echooff.de>
2023-07-16 20:47:38 +0200

    Force basic color support when running tests

--- ava.config.js
+++ ava.config.js
@@ -1,5 +1,8 @@
 export default {
     files: ['./test/unit/**/*.test.{ts,tsx}'],
+    environmentVariables: {
+        FORCE_COLOR: '1',
+    },
     typescript: {
         extensions: ['ts', 'tsx'],
         rewritePaths: {
